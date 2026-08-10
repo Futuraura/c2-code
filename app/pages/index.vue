d8410e83bd17f5f031cd42130ffbfd36b4ebc16f
Mark P. <github@yesitsme.dev>
2026-08-10 22:15:02 +0300

    feat: initialize Nuxt 4 project with ESLint and TypeScript support
    - Added ESLint configuration file for code linting.
    - Created nuxt.config.ts for Nuxt application configuration.
    - Added package.json with necessary dependencies and scripts for building and running the application.
    - Included favicon.ico for branding.
    - Added robots.txt to manage web crawler access.
    - Created tsconfig.json for TypeScript configuration and references.

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+
+    <RedTitle>This is an auto-imported component</RedTitle>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon"],
+  postcss: {
+    plugins: {},
+  },
+  app: {
+    head: {
+      title: "C2 Code",
+      htmlAttrs: {
+        lang: "en",
+      },
+      // link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
+});
